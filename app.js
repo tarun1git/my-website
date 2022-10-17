c5f5da9fa252339056abe914484321d781ddc3a1
tarun1git <tarunrajput7959@gmail.com>
2022-10-18 02:30:23 +0530

    update

---
+++ app.js
@@ -0,0 +1,11 @@
+var display = document.getElementById("sideMenu");
+var hidden = document.getElementById("closeMenu");
+var list = document.getElementById("list");
+
+display.onclick = function(){
+    list.style.visibility = "visible";
+    list.style.right = "0%";
+}
+hidden.onclick = function(){
+    list.style.right = "-50%";
+}
